ebfef8be78fe9030fd02848163250d70eddbc18f
Brian Magee <bemagee@gmail.com>
2013-08-03 17:16:06 -0400

    New CodeAcademy exercises

---
+++ CodeAcademy/Python/string_cat-55.py
@@ -0,0 +1,8 @@
+n = ["Michael", "Lieberman"]
+# Add your function here
+def join_strings(string):
+    for i in string:
+        concat = string[0] + string[1]
+    return concat
+
+print join_strings(n)
