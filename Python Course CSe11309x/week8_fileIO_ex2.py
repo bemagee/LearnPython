86b35b035aefa64e65976fdb2e05d0a6084121d7
Brian Magee <brmagee@broadcom.com>
2016-03-16 15:39:15 -0400

    first commit for git_tutorial

--- Python Course CSe11309x/week8_fileIO_ex2.py
+++ Python Course CSe11309x/week8_fileIO_ex2.py
@@ -16,10 +16,17 @@
 #             'Abigail': [84.0, 50.0, 72.0, 75.0]}
 
 
-def read_csv(file_name):
-    new_dict = open(file_name,'r')
-    for i in new_dict:
-        print (i)
-    new_dict.close()
+def _construct_dictionary_from_file_sample_(filename):
+    my_dictionary = {}
+    # Make a connection to the file
+    file_pointer = open(filename, 'r')
+    data = file_pointer.readlines()
+    for line in data:
+        name, course1, course2, course3, course4 = line.strip().split(',')
+        my_dictionary[name] = [float(course1), float(course2), float(course3), float(course4)]
+    return my_dictionary
 
-returned_dict = read_csv('week8_fileIO_ex2.txt')
+
+
+returned_dict = _construct_dictionary_from_file_sample_('week8_fileIO_ex2.txt')
+print (returned_dict)
